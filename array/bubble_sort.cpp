0cd803390d946e79759609efd6a7c7200a67f4d7
Ajeet551 <ajeet.aryan198@gmail.com>
2025-08-23 11:30:43 +0530

    Added bubble sort in array

---
+++ array/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+void bubble_sort(int *arr, int size) {
+    if (size <= 0) {
+        return;
+    }
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int size;
+    cout << "Enter a size of an array : ";
+    cin >> size;
+    int *arr = new int[size];
+    cout << "Enter the array elements : ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, size);
+    cout << "After sorting ... " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
