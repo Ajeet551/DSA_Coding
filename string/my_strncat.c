d524b63e9759338421c668ab7826cfa5fc640ad4
Ajeet551 <ajeet.aryan198@gmail.com>
2025-08-23 11:21:05 +0530

    Added my_strncat() user defined API

--- string/my_strncat.c
+++ string/my_strncat.c
@@ -2,37 +2,36 @@
 int my_strlen(const char* str) {
     int len;
     for (len = 0; str[len] != '\0'; len++);
-    return len;    
+    return len;
 }
 char* my_strncat(char* str1, const char* str2, int n) {
-    int len_1 = my_strlen(str1);
-    int i;
     if (str1[0] == '\0' || str2[0] == '\0') {
-        return NULL;
+        return (char*)str1;
     }
-    for (i = 0; i < n; i++) {
-        str1[len_1 + i] = str2[i];
+    int len = my_strlen(str1);
+    int i = 0;
+    for (; i < n; i++) {
+        str1[len + i] = str2[i];
     }
-    str1[len_1 + i] = '\0';
+    str1[len + i] = '\0';
     return (char*)str1;
 }
 int main() {
-    char str1[100];
-    char str2[100];
-    int n;
-    printf("Enter string 1: ");
+    char str1[100], str2[100];
+    int num;
+    printf("Enter string 1 : ");
     scanf("%99[^\n]", str1);
     getchar();
     printf("Enter string 2 : ");
     scanf("%99[^\n]", str2);
     getchar();
     printf("Enter a number : ");
-    scanf("%d", &n);
-    char* ptr = my_strncat(str1, str2, n);
+    scanf("%d", &num);
+    char* ptr = my_strncat(str1, str2, num);
     if (ptr != NULL) {
-        printf("Cancatinate : %s\n", ptr);
+        printf("my_strncat() : %s\n", ptr);
     } else {
-        printf("String not cancatinated !!\n");
-    }
+        printf("strnig not found !!\n");
+    }    
     return 0;
 }
