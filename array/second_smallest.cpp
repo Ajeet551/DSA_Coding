d440d30c8ce08ae386778d92b8824dce6ba56bef
Ajeet551 <ajeet.aryan198@gmail.com>
2025-08-23 22:53:52 +0530

    modified file name for second smallest in array

--- array/second_smallest.cpp
+++ array/second_smallest.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
-void second_smallest(int* arr, int size) {
-    if (size <= 0) {
-        return;
-    }
+void largest_smallest(int* arr, int size) {
+    int large = arr[0];
     int small = arr[0];
-    int small_2 = arr[1];
-    for (int i = 2; i < size; i++) {
-        if (small > arr[i]) {
-            small_2 = small;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > large) {
+            large = arr[i];
+        }
+    }
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < small) {
             small = arr[i];
         }
     }
-    cout << "1st smallest : " << small << " 2nd smallest : " << small_2 << endl;
+    cout << "Largest : " << large << " smallest : " << small << endl;
 }
 int main() {
     int size;
     cout << "Enter a size of an array : ";
     cin >> size;
-    int* arr = new int[size];
+    int *arr = new int[size];
     cout << "Enter the array elements : ";
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
-    second_smallest(arr, size);
+    largest_smallest(arr, size);
     delete []arr;
     return 0;
 }
